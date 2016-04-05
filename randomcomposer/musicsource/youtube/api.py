01607f9b8a0358f343576d178082f7d4895c89b4
Pieter De Praetere <pieter.de.praetere@helptux.be>
2016-04-05 23:32:37 +0200

    Added API

---
+++ randomcomposer/musicsource/youtube/api.py
@@ -0,0 +1,7 @@
+from randomcomposer.jsonapi.apirequest.request import ApiRequest
+from randomcomposer.jsonapi.apiresponse.response import ApiResponse
+
+
+class YoutubeApi:
+    def __init__(self):
+        pass
